d34eda235b10265309603d302f7a938bc32346e5
Madhav M <madhavmanoj117@gmail.com>
2021-03-29 15:45:15 +0530

    update config to use local version of Xft

--- config.mk
+++ config.mk
@@ -7,16 +7,21 @@ VERSION = 0.8.4
 PREFIX = $(HOME)/.local
 MANPREFIX = $(PREFIX)/share/man
 
+HOMELIB = $(HOME)/.local/lib
+HOMEINC = $(HOME)/.local/include
+
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
 PKG_CONFIG = pkg-config
 
 # includes and libs
-INCS = -I$(X11INC) \
+INCS = -I$(HOMEINC) \
+       -I$(X11INC) \
        `$(PKG_CONFIG) --cflags fontconfig` \
        `$(PKG_CONFIG) --cflags freetype2`
-LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft \
+LIBS = -L$(HOMELIB) -Wl,-rpath,$(HOMELIB) -lXft \
+       -L$(X11LIB) -lm -lrt -lX11 -lutil \
        `$(PKG_CONFIG) --libs fontconfig` \
        `$(PKG_CONFIG) --libs freetype2`
 
